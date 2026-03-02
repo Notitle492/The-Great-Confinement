077c608c8dcdfc90adfe6e7cdc756fefdae09b28
Notitle <aesirusi@gmail.com>
2026-03-02 21:34:34 +0800

    改程式碼之類(然後改英聽黑幕Panel)

--- Assets/Scripts/Audio/Music/MusicManager.cs
+++ Assets/Scripts/Audio/Music/MusicManager.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     public static MusicManager Instance;
-    [SerializeField]
-    private MusicLibrary musicLibrary;
-    [SerializeField]
-    private AudioSource musicSource;
+    [SerializeField] private MusicLibrary musicLibrary;
+    [SerializeField] private AudioSource musicSource;
 
     private void Awake()
     {
@@ -23,9 +22,53 @@ public class MusicManager : MonoBehaviour
         }
     }
 
+    
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "EnglishListeningTest")
+        {
+            StopMusic(1.0f); // 在聽力測試場景自動淡出並停止
+        }
+    }
+
     public void PlayMusic(string trackName, float fadeDuration = 0.5f)
     {
-        StartCoroutine(AnimateMusicCrossfade(musicLibrary.GetClipFromName(trackName), fadeDuration));
+        
+        AudioClip clip = musicLibrary.GetClipFromName(trackName);
+        if (clip != null)
+        {
+            StartCoroutine(AnimateMusicCrossfade(clip, fadeDuration));
+        }
+    }
+
+    public void StopMusic(float fadeDuration = 0.5f)
+    {
+        StartCoroutine(AnimateMusicFadeOut(fadeDuration));
+    }
+
+    IEnumerator AnimateMusicFadeOut(float fadeDuration)
+    {
+        float startVolume = musicSource.volume;
+        float percent = 0;
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * 1 / fadeDuration;
+            musicSource.volume = Mathf.Lerp(startVolume, 0, percent);
+            yield return null;
+        }
+        musicSource.Stop();
     }
 
 
